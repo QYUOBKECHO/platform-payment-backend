bd602460340430cd7e8186d0dcbcecfbb8edb246
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-09-04 19:18:35 +0100

    init commit

---
+++ src/main/java/org/engripaye/platformpaymentbackend/PlatformPaymentBackendApplication.java
@@ -0,0 +1,13 @@
+package org.engripaye.platformpaymentbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformPaymentBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlatformPaymentBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/engripaye/platformpaymentbackend/PlatformPaymentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.engripaye.platformpaymentbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformPaymentBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
