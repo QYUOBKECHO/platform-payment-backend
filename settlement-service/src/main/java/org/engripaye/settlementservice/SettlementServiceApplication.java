abe33f3276d7e666c1a1f25af52286506837a17f
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-09-04 20:48:34 +0100

    backend service commit by ENGR. IPAYE

--- settlement-service/src/main/java/org/engripaye/settlementservice/SettlementServiceApplication.java
+++ settlement-service/src/main/java/org/engripaye/settlementservice/SettlementServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.payment.settlement.feign")
 public class SettlementServiceApplication {
 
     public static void main(String[] args) {
--- settlement-service/src/main/java/org/engripaye/settlementservice/feign/TransactionClient.java
+++ settlement-service/src/main/java/org/engripaye/settlementservice/feign/TransactionClient.java
@@ -1,5 +1,4 @@
 package org.engripaye.settlementservice.feign;
-
 import org.engripaye.settlementservice.dto.TransactionDTO;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
